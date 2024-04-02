f364c5b52636da0d0280788584c93f37b76257ae
akshay <akshaychandra418@gmail.com>
2024-04-02 11:34:26 +0530

    adding body parser

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 import net from "net";
 
 const server = net.createServer((socket) => {
-  // 'connection' listener.
   console.log("client connected");
   socket.on("end", () => {
     console.log("client disconnected");
   });
-
-  socket.on("close", () => {
-    console.log("Client disconnected before receiving full response");
-    // Handle disconnection (e.g., stop sending data)
-  });
-  //   socket.write("hello\r\n");
 
   socket.on("data", (data) => {
     parseData(data);
@@ -22,10 +15,10 @@ const server = net.createServer((socket) => {
   //   socket.write("\r\n"); // Empty line to separate headers and body
   //   socket.write("Hello from server!\r\n");
 
-  socket.write("HTTP/1.1 200 OK\r\n");
-  socket.write("Content-Type: application/json\r\n");
-  socket.write("\r\n");
-  socket.write('{"name":"babu"}\r\n');
+  //   socket.write("HTTP/1.1 200 OK\r\n");
+  //   socket.write("Content-Type: application/json\r\n");
+  //   socket.write("\r\n");
+  //   socket.write('{"name":"babu"}\r\n');
   //   socket.pipe(socket);
 });
 server.on("error", (err) => {
@@ -38,20 +31,33 @@ server.listen(3000, () => {
 function parseData(input) {
   console.log("inside the function");
   const data = input.toString();
-  console.log(data);
+  // console.log(data);
   const [firstline, remaining] = firstLineParser(data);
-  const [headers, body] = headerParser(remaining);
-  //   const bodyData = JSON.parse(body);
-  //   console.log(bodyData);
+  const [method, path, httpVersion] = firstline.split(" ");
+
+  const [headers, body] = headerSplitter(remaining);
+  const result = headerParser(headers);
+
+  if (result.hasOwnProperty("Content-Length"))
+    bodyParser(body, result["Content-Type"]);
+
   console.log("outside the function");
 }
 
-function firstLineParser(input) {
-  const parsedData = input.split(/\r?\n/);
-  return [parsedData[0], input.slice(parsedData[0].length).trim()];
-}
+const firstLineParser = (input) => [
+  input.split(/\r?\n/)[0],
+  input.slice(input.split(/\r?\n/)[0].length).trim(),
+];
 
-function headerParser(input) {
-  const parsedData = input.split(/(\r?\n){2}/);
-  return [parsedData[0], input.slice(parsedData[0].length).trim()];
+const headerSplitter = (input) => [
+  input.split(/(\r?\n){2}/)[0],
+  input.slice(input.split(/(\r?\n){2}/)[0].length).trim(),
+];
+
+const headerParser = (input) =>
+  Object.fromEntries(input.split("\r\n").map((string) => string.split(":")));
+
+function bodyParser(body, contentType) {
+  console.log(body);
+  console.log(contentType);
 }
