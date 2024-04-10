0e3dde60a0af44277b7858e7e819c977cc6b2a10
AkshayCdr <akshaychandra418@gmail.com>
2024-04-10 11:36:51 +0530

    removed unwanted files

--- test.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-
-// (async () => {
-//   const data = await fs.readFile("/text.txt", "utf8");
-//   console.log(data);
-// })();
-
-const data = fs.readFileSync("./public/index.html").toString();
-console.log(data);
