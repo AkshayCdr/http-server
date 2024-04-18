200b4ad7b8acfe80db92f17fc12a6dbe9f956841
AkshayCdr <akshaychandra418@gmail.com>
2024-04-18 15:42:00 +0530

    fixed gitignore

--- public/script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector(".hai");
-
-const file = document.querySelector(".file");
-const button = document.querySelector(".btn");
-
-// button.addEventListener("click", (e) => {
-//   e.preventDefault();
-// });
